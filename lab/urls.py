954c3972ee93f16e8bdd47bd0fae7315a0fb7a2d
Uday Kiran <udayjyo02@gmail.com>
2022-11-26 10:02:58 +0530

    answers are displayed to host

--- lab/urls.py
+++ lab/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('add_answer/<str:pk>', views.add_answer, name='add_answer'),
     path('add_participants/<str:pk>', views.add_participants, name='add_participants'),
     path('create_poll/<str:pk>', views.create_poll, name='create_poll'),
-    path('compiler_submit/<str:pk>', views.submit_code, name='compiler_submit'),
+    path('compiler_submit/<str:pk>/(?P<id>\d+)/$', views.submit_code, name='compiler_submit'),
     path('compiler_run/<str:pk>', views.run_code, name='compiler_run'),
 ]
